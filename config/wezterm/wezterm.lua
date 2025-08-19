344163d0e7d5101e4299158a7ada82d03f4b7811
Li Zihao from macos <lizihaojp@outlook.com>
2025-08-19 22:20:27 +0900

    add .gitignore

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -10,32 +10,20 @@ config = {
     enable_tab_bar = true,
     hide_tab_bar_if_only_one_tab = true,    -- Hide the tab bar when there is only one tab
     window_close_confirmation = "NeverPrompt",
-    window_decorations = "NONE", -- disable the title bar but enable the resizable border
+    window_decorations = "RESIZE", -- disable the title bar but enable the resizable border
     font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
-    font_size = 12,
+    font_size = 12.5,
     color_scheme = "DanQing (base16)",
     default_cursor_style = 'BlinkingBlock',
     macos_window_background_blur = 25, -- Enable window background blur on macOS
     background = {
         {
             source = {
-                File = home .. "/dotfiles/config/Arknight_Personal_Terminal.png",
-            },
-            hsb = {
-                hue = 1.0,
-                saturation = 1.02,
-                brightness = 0.25
-            },
-            horizontal_align = "Center",
-            vertical_align = "Middle",
-        },
-        {
-            source = {
                 Color = "#282c35", -- dark purple
             },
             width = "100%",
             height = "100%",
-            opacity = 0.55,
+            opacity = 0.75,
         },
     },
     window_padding = {
