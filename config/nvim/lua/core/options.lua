b2e35cf8d2cdbaeaf287bd44952f81c05e3cb030
Li Zihao from macos <lizihaojp@outlook.com>
2025-08-23 22:18:35 +0900

    change colorscheme

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -6,7 +6,7 @@ vim.opt.mouse = 'a' -- allow the mouse to be used in Nvim
 -- Tab
 vim.opt.tabstop = 4 -- number of visual spaces per TAB
 vim.opt.softtabstop = 4 -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
+vim.opt.shiftwidth = 0 -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
 
 -- UI config
@@ -17,6 +17,7 @@ vim.opt.splitbelow = true -- open new vertical split bottom
 vim.opt.splitright = true -- open new horizontal splits right
 vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
 vim.opt.showmode = false -- we are experienced, wo don't need the "-- INSERT --" mode hint
+vim.opt.colorcolumn = "80"
 
 -- Searching
 vim.opt.incsearch = true -- search as characters are entered
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,41 +1,39 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-        
-    opts = {
-      transparent_background = true,
-      custom_highlights = function(colors)
-        -- stylua: ignore
-        return {
-          LineNr     = { fg = colors.surface2 },
-          Visual     = { bg = colors.overlay0 },
-          Search     = { bg = colors.surface2 },
-          IncSearch  = { bg = colors.mauve },
-          CurSearch  = { bg = colors.mauve },
-          MatchParen = { bg = colors.mauve, fg = colors.base, bold = true },
-        }
-      end,
-      integrations = {
-        barbar = true,
-        blink_cmp = true,
-        gitsigns = true,
-        mason = true,
-        noice = true,
-        nvimtree = true,
-        rainbow_delimiters = true,
-        snacks = {
-          enabled = true,
-          indent_scope_color = "lavender", -- catppuccin color (eg. `lavender`) Default: text
-        },
-        which_key = true,
-        flash = true,
-        lsp_trouble = true,
-      },
+    {
+        "catppuccin/nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd([[
+                colorscheme catppuccin-mocha
+
+                " Enable transparency
+                highlight Normal guibg=NONE ctermbg=NONE
+                highlight NormalNC guibg=NONE ctermbg=NONE
+                highlight LineNr guibg=NONE ctermbg=NONE
+                highlight SignColumn guibg=NONE ctermbg=NONE
+                highlight FoldColumn guibg=NONE ctermbg=NONE
+                highlight CursorLine guibg=NONE ctermbg=NONE
+                
+                " For nvim tree
+                highlight NvimTreeIndentMarker guifg=#A6E3A1
+                highlight NvimTreeFolderArrowClosed guifg=#A6E3A1
+                highlight NvimTreeFolderArrowOpen guifg=#A6E3A1
+                highlight NvimTreeNormal guibg=NONE
+
+                " For vertical split window border
+                highlight WinSeparator guifg=#A6E3A1
+
+                " For Diagnostic mesgs
+                highlight DiagnosticVirtualTextError guibg=NONE
+                highlight DiagnosticVirtualTextWarn guibg=NONE
+                highlight DiagnosticVirtualTextInfo guibg=NONE
+                highlight DiagnosticVirtualTextHint guibg=NONE
+                highlight DiagnosticVirtualTextOk guibg=NONE
+
+                " For float terms like hoverdoc
+                highlight NormalFloat guibg=NONE
+
+                ]])
+        end,
     },
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+}
