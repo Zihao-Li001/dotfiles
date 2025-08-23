12b344b65c0859b8b970b9c486c23e820ec1fe52
Li Zihao <li.zihao24phd@gmail.com>
2025-08-23 22:28:52 +0900

    bug?

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -3,22 +3,22 @@ return{
         "folke/trouble.nvim",
         cmd = "Trouble",
         opts = {
-          modes = {
-            preview_float = {
-              mode = "diagnostics",
-              preview = {
-                type = "float",
-                relative = "editor",
-                border = "rounded",
-                title = "Preview",
-                title_pos = "center",
-                position = { 0, -2 },
-                size = { width = 0.3, height = 0.3 },
-                zindex = 200,
-              },
-            },
-          },
-        }, 
+          -- modes = {
+          --   preview_float = {
+          --     mode = "diagnostics",
+          --     preview = {
+          --       type = "float",
+          --       relative = "editor",
+          --       border = "rounded",
+          --       title = "Preview",
+          --       title_pos = "center",
+          --       position = { 0, -2 },
+          --       size = { width = 0.3, height = 0.3 },
+          --       zindex = 200,
+          --     },
+          --   },
+          -- },
+        },
         keys = {
             {
                 "<leader>xx",
@@ -64,5 +64,4 @@ return{
         -- },
         config = true,
     },
-        
 }
